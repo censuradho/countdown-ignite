5578a67888a6efdcc2f30e82bb13e40c63b0ace3
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-10-30 09:25:51 -0300

    feat(history): add table style

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -3,7 +3,43 @@ import * as Styles from './styles'
 export function History () {
   return (
     <Styles.Container>
-      <h1>History</h1>
+      <h1>Meu histórico</h1>
+      <Styles.HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>à 2 dias</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>à 2 dias</td>
+              <td>Concluído</td>
+            </tr>            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>à 2 dias</td>
+              <td>Concluído</td>
+            </tr>            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>à 2 dias</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </Styles.HistoryList>
     </Styles.Container>
   )
 }
--- src/pages/history/styles.ts
+++ src/pages/history/styles.ts
@@ -1,3 +1,57 @@
 import { styled } from '@/stitches.config'
 
-export const Container = styled('main', {})
+export const Container = styled('main', {
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  h1: {
+    fontSize: '1.5rem'
+  },
+
+  '@table-min': {
+    padding: '3.5rem'
+  }
+})
+
+export const HistoryList = styled('div', {
+  width: '100%',
+  flex: 1,
+  overflowX: 'auto',
+
+  table: {
+    width: '100%',
+    borderCollapse: 'collapse',
+
+    th: {
+      backgroundColor: '$background',
+      padding: '1rem',
+      textAlign: 'left',
+      fontSize: '0.875rem',
+      lineHeight: '1.6',
+
+      '&:first-child': {
+        borderTopLeftRadius: '$sm',
+        paddingLeft: '1.5rem'
+      },
+      '&:last-child': {
+        borderTopRightRadius: '$sm',
+        paddingRight: '1.5rem'
+      }
+    },
+
+    td: {
+      backgroundColor: '$background',
+      padding: '1rem',
+      fontSize: '0.875rem',
+      lineHeight: '1.6',
+
+      '&:first-child': {
+        width: '50%',
+        paddingLeft: '1.5rem'
+      },
+      '&:last-child': {
+        paddingRight: '1.5rem'
+      }
+    }
+  }
+})
